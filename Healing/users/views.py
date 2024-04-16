867bd61fcdaed52b163fe2a57cc4f18eec1cd0b0
Ricardo Kelvin <rickelvin97@outlook.com>
2024-04-16 07:05:49 -0300

    feat: finish registration method

--- Healing/users/views.py
+++ Healing/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def register(request):
     if request.method == "GET":
@@ -12,18 +14,25 @@ def register(request):
         password_confir = request.POST.get('password_confir')
         
         if password != password_confir:
-            print("Passwords don't match, bitch...")
+            messages.add_message(request, constants.ERROR, 'As senhas digitadas não são iguais, tente novamente.')
             return redirect('/users/register/')
         
         if len(password) < 6:
-            print('Password is too small, like your dick, asshole.')
+            messages.add_message(request, constants.ERROR, 'A senha deve possuir pelo menos 6 caracteres')
             return redirect('/users/register/')
         
         users = User.objects.filter(username=username)
         print(users.exists())
         
         if users.exists():
-            print('This name has already been taken, choose another one.')
+            messages.add_message(request, constants.ERROR, 'Alguém já pegou este nome de usuário, tente algum outro.')
+            return redirect('/users/register/')
+        
+        users = User.objects.filter(email=email)
+        print(users.exists())
+        
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'O e-mail que vocês está tentando registrar já pertence a alguém, tente algum outro.')
             return redirect('/users/register/')
         
         user = User.objects.create_user(
@@ -32,5 +41,5 @@ def register(request):
             password=password
         )
         
-        return redirect('/users/login/')
+        return redirect('/users/register/')
     
